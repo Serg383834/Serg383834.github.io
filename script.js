75045f0dbe429172e28f13f8ec15eb9d1137f439
Serg383834 <serg383834@yandex.ru>
2022-04-10 00:37:05 +0300

    Done

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 
 $(function(){	
+
+	// маска ввода телефона	
+	$('input[id= phone]').mask('+7 (999) 999-99-99')	
+
 	//переключатель доклада
 	$("input[name = 'doclad']").change(function(){
 		if($(this).val() === "yes"){
@@ -10,4 +14,11 @@ $(function(){
 			$("input[name = 'doclad_name']").prop("disabled","disabled");
 		}
 	});
+	//отправка формы - заглушка
+	$(".reg_form").submit(function(){
+		alert("Форма отправлена");
+		return false;
+	});
 })
+
+
