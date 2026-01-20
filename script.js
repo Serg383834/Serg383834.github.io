7cef9778c315736ecc37bb384ddf14a65831304f
Serg383834 <71527273+Serg383834@users.noreply.github.com>
2026-01-20 22:43:14 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ $(function(){
 	$('input[id= phone]').mask('+7 (999) 999-99-99')	
 
 	//переключатель доклада
-	$("input[name = 'doclad']").change(function(){
+	$("input[name = 'report']").change(function(){
 		if($(this).val() === "yes"){
-			$(".doclad_line").stop().show();
-			$("input[name = 'doclad_name']").prop("disabled",false).removeProp("disabled");
+			$(".report_line").stop().show();
+			$("input[name = 'report_name']").prop("disabled",false).removeProp("disabled");
 		}else{
-			$(".doclad_line").stop().hide();
-			$("input[name = 'doclad_name']").prop("disabled","disabled");
+			$(".report_line").stop().hide();
+			$("input[name = 'report_name']").prop("disabled","disabled");
 		}
 	});
 	//отправка формы - заглушка
